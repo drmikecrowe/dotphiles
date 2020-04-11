3de4d2b94172572b0b6feb7b700bd49a7ac99d6b
Mike Crowe <drmikecrowe@gmail.com>
2020-04-11 12:54:18 -0400

    Tweaking some dotfiles

--- bash_it/custom/environment.bash
+++ bash_it/custom/environment.bash
@@ -10,6 +10,8 @@ if [ "$(which ksshaskpass)" != "" ]; then
   export SSH_ASKPASS="$(which ksshaskpath)"
 fi
 
-[[ ":$PATH:" != *":/$HOME/bin:"* ]] && export PATH="$PATH:/$HOME/bin"
-[[ ":$PATH:" != *":/$HOME/.local/bin:"* ]] && export PATH="$PATH:/$HOME/.local/bin"
+[[ ":$PATH:" != *":$HOME/bin:"* ]] && export PATH="$PATH:$HOME/bin"
+[[ ":$PATH:" != *":$HOME/.local/bin:"* ]] && export PATH="$PATH:$HOME/.local/bin"
+[[ ":$PATH:" != *":$HOME/.pyenv/bin:"* ]] && export PATH="$PATH:$HOME/.pyenv/bin"
+[[ ":$PATH:" != *":$HOME/.nodenv/bin:"* ]] && export PATH="$PATH:$HOME/.nodenv/bin"
 [[ ":$PATH:" != *":/snap/bin:"* ]] && export PATH="$PATH:/snap/bin"
